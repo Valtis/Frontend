fa0fbe088f92fe4406dba52d6d4cfcd9123ebe34
Valtis <erkka.kaaria@helsinki.fi>
2014-12-24 19:56:40 +0200

    Some initial testing and learning basic language constructs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use token::TokenType;
+use token::TokenSubType;
+use token::SyntaxToken;
 
-
+mod token;
 
 fn main() {
-  println!("Hello world!");
+  let foo = SyntaxToken::new(TokenType::Number, TokenSubType::DoubleNumber, "32421.1".to_string());
 
+  let equals = TokenType::Number == TokenType::Text;
+  let equals2 = TokenType::Number == TokenType::Number;
+
+  println!("Foo: {}", foo);
+  println!("{}, {}", equals, equals2);
 }
